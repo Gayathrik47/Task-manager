2a84eb3445f4882ba643ef6eb72695dc343b5ba1
Gayathrik47 <gayathrik2299@gmail.com>
2026-04-23 20:51:42 +0530

    fix mongodb env

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://gayathri_k:gayu123@cluster0.3qehc5r.mongodb.net/taskdb")
-.then(() => console.log("DB connected"))
-.catch(err => console.log(err));
+//  TEMP: direct MongoDB connection (so it works now)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
 
+// test route
 app.get("/", (req, res) => {
-  res.send("Server working 💙");
+  res.send("Server working ");
 });
 
-
+// routes
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running");
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
-
